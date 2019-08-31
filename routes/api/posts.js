cc9f44b6eece4cf76b507c077f0c09a3c914ab01
Gerardo Garcia <geraz26@hotmail.com>
2019-08-31 15:30:04 -0500

    Obtener todos los post, post por id y eliminar post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -50,4 +50,74 @@ router.post(
   }
 );
 
+// @route   GET api/posts
+// @desc    Get all post
+// @access  Private
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   GET api/posts/:post_id
+// @desc    Get post by ID
+// @access  Private
+
+router.get("/:post_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   DELETE api/posts/:post_id
+// @desc    DELETE post by ID
+// @access  Private
+
+router.delete("/:post_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    // Verificar si el usuario que desea eliminar el post es el "dueño"
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    // Eliminar el post si el usuario es el dueño
+    await post.remove();
+
+    res.json({ msg: "Post Removed" });
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
