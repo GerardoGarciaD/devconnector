4c7f72773f9e2001adf79f71a5961198fec1c009
Gerardo Garcia <geraz26@hotmail.com>
2019-08-31 16:42:00 -0500

    Añadir y eliminar comentarios

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -177,4 +177,94 @@ router.put("/unlike/:post_id", auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/posts/comment/:post_id
+// @desc    Comment on a post
+// @access  Private
+
+router.post(
+  "/comment/:post_id",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    // Se verifica que no haya errores con los campos requeridos
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      // Se obtiene el usuario mediante el id que se recibe del token y se quita la contraseña
+      const user = await User.findById(req.user.id).select("-password");
+      // se obtiene el opst con el id que se manda desde la URL
+      const post = await Post.findById(req.params.post_id);
+
+      // Se crea un nuevo objeto que contiene la informacion del comentario
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: user.id
+      };
+
+      // Se añade el objeto creado al principio del array
+      post.comments.unshift(newComment);
+      //   Se guarda el post en la base de datos
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route   PUT api/posts/comment/:post_id/:comment_id
+// @desc    Delete a comment
+// @access  Private
+
+router.delete("/comment/:post_id/:comment_id", auth, async (req, res) => {
+  try {
+    // Se obtiene el post mediante el id que se pasa en la URL (se utiliza el modelo de datos POST)
+    const post = await Post.findById(req.params.post_id);
+
+    // Se busca el comentario a eliminar (si es que existe) mediante el id del comentario que se manda desde la URL
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+
+    // Verificar que el comentario existe
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment does not exists" });
+    }
+
+    // Verificar que quien desea eliminar el comentario sea el "dueño"
+
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    // Se obtiene el id del usuario que le dio like al post
+    const removeIndex = post.comments
+      .map(comment => comment.user.toString())
+      .indexOf(req.user.id);
+
+    // Se elimina el comentario del array con el indice encontrado
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
