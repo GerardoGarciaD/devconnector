ea8bff93d7bcf9bcf0c298c7717b31d16057b3f4
Gerardo Garcia <geraz26@hotmail.com>
2019-09-01 21:34:08 -0500

    Componente login con Hooks

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const { email, password } = formData;
+
+  const onChange = async e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  // Esto es una funcion async por que dentro ocurren acciones y peticiones al servidor
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log("SUCCESS");
+  };
+
+  return (
+    <>
+      <div className="alert alert-danger">Invalid credentials</div>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign into Your Account
+      </p>
+      <form className="form" action="dashboard.html">
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </>
+  );
+};
+
+export default Login;
