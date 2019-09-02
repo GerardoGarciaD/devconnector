8e71f2b1b5d33b262efb7d449af26d8bdd35c816
Gerardo Garcia <geraz26@hotmail.com>
2019-09-01 21:34:27 -0500

    Componente Register con Hooks

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
+
+const Register = ({ setAlert }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConf: ""
+  });
+
+  const { name, email, password, passwordConf } = formData;
+
+  const onChange = async e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  // Esto es una funcion async por que dentro ocurren acciones y peticiones al servidor
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (password !== passwordConf) {
+      setAlert("Password do not match", "danger");
+    } else {
+      console.log("Success");
+    }
+  };
+
+  return (
+    <>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={e => onChange(e)}
+            minLength="6"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="passwordConf"
+            value={passwordConf}
+            onChange={e => onChange(e)}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </>
+  );
+};
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { setAlert }
+)(Register);
