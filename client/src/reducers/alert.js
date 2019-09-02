1ba0137a5f050a0dca2e3baf8b3170941344dd83
Gerardo Garcia <geraz26@hotmail.com>
2019-09-01 21:41:49 -0500

    Reducer para las alertas

---
+++ client/src/reducers/alert.js
@@ -0,0 +1,19 @@
+import { SET_ALERT, REMOVE_ALERT } from "../actions/types";
+
+const intialState = [];
+
+export default function(state = intialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case SET_ALERT:
+      return [...state, payload];
+      break;
+
+    case REMOVE_ALERT:
+      return state.filter(alert => alert.id !== payload);
+
+    default:
+      return state;
+  }
+}
