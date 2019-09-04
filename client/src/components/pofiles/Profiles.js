dda99c0d62cadd5e231b81540e71ec6421f6c789
Gerardo Garcia <geraz26@hotmail.com>
2019-09-04 16:48:27 -0500

    Componente para mostrar todos los perfiles

---
+++ client/src/components/pofiles/Profiles.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layouts/Spinner";
+import { getProfiles } from "../../actions/profile";
+import ProfileItem from "./ProfileItem";
+
+const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
+  // Se utiliza useEffect para simular componendDidMount
+  useEffect(() => {
+    getProfiles();
+  }, [getProfiles]);
+
+  return (
+    <>
+      {loading ? (
+        <Spinner></Spinner>
+      ) : (
+        <>
+          {" "}
+          <h1 className="large text-primary"> Developers</h1>{" "}
+          <p className="lead">
+            <i className="fab fa-connectdevelop"></i> Browse and connect with
+            developers
+          </p>{" "}
+          <div className="profiles">
+            {profiles.length > 0 ? (
+              profiles.map(profile => (
+                <ProfileItem key={profile._id} profile={profile}></ProfileItem>
+              ))
+            ) : (
+              <h4>No profiles found...</h4>
+            )}
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profileReducer
+});
+
+export default connect(
+  mapStateToProps,
+  { getProfiles }
+)(Profiles);
