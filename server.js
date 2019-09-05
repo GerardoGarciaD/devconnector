ae2383aebb6388f527132519bb38b63254cba8d5
Gerardo Garcia <geraz26@hotmail.com>
2019-09-05 15:08:21 -0500

    Configuraciones para produccion

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 // Se inicializa la aplicacion con express
 const app = express();
@@ -11,13 +12,20 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
-
 // Definir rutas
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+if (process.env.NODE_ENV === "production") {
+  // Seleccionar el folder por default
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname), client, build, "index.html");
+  });
+}
 
 // Se crea la variable PORT que se encarga de escuchar si es que se esta en produccion o si no, la aplicacion se ejecuta
 // en el puerto 5000
