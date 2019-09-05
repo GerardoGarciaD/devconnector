b0b27e3520d3053d14fddeb35f94b52b2e0b4226
Gerardo Garcia <geraz26@hotmail.com>
2019-09-05 14:55:39 -0500

    Se añade el reducer para los posts

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import alert from "./alert";
 import auth from "./auth";
 import profileReducer from "./profileReducer";
+import post from "./post";
 
 export default combineReducers({
   alert,
   auth,
-  profileReducer
+  profileReducer,
+  post
 });
